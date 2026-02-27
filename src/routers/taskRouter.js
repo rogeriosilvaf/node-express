d58d1715ea03ad7752883e6ed5ef94f011ece3c4
rogeriopedrof <rogeriopedro.aa@gmail.com>
2026-02-27 20:07:42 -0300

    breve correcao

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createTaskController, getAllTasksController, updateTaskController } from "../controllers/taskController.js";
+import { createTaskController, deleteTaskController, getAllTasksController, updateTaskController } from "../controllers/taskController.js";
 
 const router = Router();
 
 router.get('/', getAllTasksController);
 router.post('/', createTaskController);
 router.patch('/:taskId', updateTaskController);
-router.delete('/:taskId', )
+router.delete('/:taskId', deleteTaskController);
 
 export default router;
